f4468848ded1ad13bc67950af7c0f6d7d35ffa41
Noris-0 <sanyuemyname@gmail.com>
2025-11-18 14:57:38 -0800

    fixed index problem

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const Starting = document.getElementById("startbutton");
     Starting.addEventListener("click", function() {
-        window.location.href = "../Warning/warning.html";
+        window.location.href = "/Warning/warning.html";
     })
 });
 
